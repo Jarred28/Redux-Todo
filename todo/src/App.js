f87c0e7b4422aff5ff2633bc558bb29cfa841a90
jarred richards <jarred2828@gmail.com>
2019-05-15 16:25:12 -0400

    ListState,TodoList&TodoComponentsCreated

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import TodoListContainer from './containers/TodoListContainer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Jarred's Todo List</h1>
+        <h1>Andrews Todo List</h1>
+        <TodoListContainer/>
       </div>
     );
   }
