0e1c2b530470a0e7fc55562d39e4aa2f9cff0700
jarred richards <jarred2828@gmail.com>
2019-05-15 17:01:20 -0400

    MVP

--- todo/src/App.js
+++ todo/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Andrews Todo List</h1>
+        <h1>Jarred's Todo List</h1>
         <TodoListContainer/>
       </div>
     );
