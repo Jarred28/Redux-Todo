03a5871c746a3f39a6ee03d164d1b9aab52d2e65
jarred richards <jarred2828@gmail.com>
2019-05-15 16:46:52 -0400

    Input&AddedFunctionalityToListContainers

--- todo/src/containers/TodoListContainer.js
+++ todo/src/containers/TodoListContainer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 import { addTodo, toggleTodo } from '../store/actions';
+import TodoList from '../components/todoList/TodoList';
+import TodoInput from '../components/todoList/TodoInput';
 
 class TodoListContainer extends React.Component {
 
@@ -8,10 +10,31 @@ class TodoListContainer extends React.Component {
 		todoInputText: ''
 	};
 
+	onChange = e => {
+		e.preventDefault();
+		this.setState({
+			todoInputText: e.target.value
+		});
+	};
+
+	addNewTodo = () => {
+		this.props.addTodoOnProps(this.state.todoInputText);
+		this.setState({ todoInputText: '' });
+	};
+
+	toggleCompleted = index => {
+		this.props.toggleTodo(index);
+	};
+
 	render() {
 		return(
 			<div>
-
+				<TodoInput 
+					addTodo={this.addNewTodo}
+					inputText={this.state.todoInputText}
+					onInputChange={this.onChange}
+				/>
+				<TodoList todos={this.props.todosOnProps} toggleCompleted={this.toggleCompleted} />
 			</div>
 		)
 	}
